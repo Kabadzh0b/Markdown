68b04d5e79238766b13e6c8b0f9bcb456442e40c
Kabadzh0b <chubbudda@gmail.com>
2024-02-29 21:22:09 +0200

    Revert "Wrong commit (to show revert commit)"
    This reverts commit 2ad3e5f0759f188657062fee87867f8dfa6aa8d2.

--- markdown.js
+++ markdown.js
@@ -233,9 +233,6 @@ const getFullFilePath = (basicPath) => {
     return path.join(__dirname, basicPath)
 }
 
-console.log('Oh no! Noting works anymore!')
-return
-
 const ARGS = process.argv
 
 const OUT_INDEX = ARGS.indexOf('--out')
